f546bbf0285eebbdac460fbfccece284a87663ad
Pierre Gleize <gleize.pierre@gmail.com>
2025-11-13 23:01:13 +0000

    Downstream update.

--- notebook/inference.py
+++ notebook/inference.py
@@ -25,6 +25,7 @@ from copy import deepcopy
 from kaolin.visualize import IpyTurntableVisualizer
 from kaolin.render.camera import Camera, CameraExtrinsics, PinholeIntrinsics
 import builtins
+from pytorch3d.transforms import quaternion_multiply, quaternion_invert
 
 import sam3d_objects  # REMARK(Pierre) : do not remove this import
 from sam3d_objects.pipeline.inference_pipeline_pointmap import InferencePipelinePointMap
@@ -94,17 +95,24 @@ class Inference:
         check_hydra_safety(config, WHITELIST_FILTERS, BLACKLIST_FILTERS)
         self._pipeline: InferencePipelinePointMap = instantiate(config)
 
+    def merge_mask_to_rgba(self, image, mask):
+        mask = mask.astype(np.uint8) * 255
+        mask = mask[..., None]
+        # embed mask in alpha channel
+        rgba_image = np.concatenate([image[..., :3], mask], axis=-1)
+        return rgba_image
+
     def __call__(
         self,
         image: Union[Image.Image, np.ndarray],
-        mask: Optional[Union[None, Image.Image, np.ndarray]] = None,
+        mask: Optional[Union[None, Image.Image, np.ndarray]],
         seed: Optional[int] = None,
         pointmap=None,  # TODO(Pierre) : add pointmap type
     ) -> dict:
-        # enable or disable layout model
+        image = self.merge_mask_to_rgba(image, mask)
         return self._pipeline.run(
             image,
-            mask,
+            None,
             seed,
             stage1_only=False,
             with_mesh_postprocess=False,
@@ -155,41 +163,7 @@ def _yaw_pitch_r_fov_to_extrinsics_intrinsics(yaws, pitchs, rs, fovs):
     return extrinsics, intrinsics
 
 
-def render_video_ring(
-    sample,
-    resolution=512,
-    bg_color=(0, 0, 0),
-    num_frames=300,
-    r=2.0,  # radius of the ring
-    pitch_deg=15,  # elevation angle above the xy-plane
-    fov=40,
-    **kwargs,
-):
-    # 1) angular positions around the ring
-    yaws = torch.linspace(0, 2 * torch.pi, num_frames).tolist()
-
-    # 2) constant pitch for every frame
-    pitch = torch.full((num_frames,), torch.deg2rad(torch.tensor(pitch_deg))).tolist()
-
-    # 3) look-at cameras centred at (0,0,0)
-    extrinsics, intrinsics = _yaw_pitch_r_fov_to_extrinsics_intrinsics(
-        yaws,
-        pitch,
-        r,
-        fov,
-    )
-
-    # 4) render
-    return render_utils.render_frames(
-        sample,
-        extrinsics,
-        intrinsics,
-        {"resolution": resolution, "bg_color": bg_color, "backend": "gsplat"},
-        **kwargs,
-    )
-
-
-def render_video_flat(
+def render_video(
     sample,
     resolution=512,
     bg_color=(0, 0, 0),
@@ -245,18 +219,40 @@ def _fix_gaussian_alignment(scene_gs, in_place=False):
     return scene_gs
 
 
-def normalized_gaussian(scene_gs, in_place=False):
+def normalized_gaussian(scene_gs, in_place=False, outlier_percentile=None):
     if not in_place:
         scene_gs = deepcopy(scene_gs)
 
-    orig_xyz = scene_gs._xyz
-    orig_scale = scene_gs._scaling
+    orig_xyz = scene_gs.get_xyz
+    orig_scale = scene_gs.get_scaling
 
-    norm_scale = orig_scale / (orig_xyz.max(dim=0)[0] - orig_xyz.min(dim=0)[0]).max()
-    norm_xyz = orig_xyz / (orig_xyz.max(dim=0)[0] - orig_xyz.min(dim=0)[0]).max()
-    norm_xyz = norm_xyz - norm_xyz.min(dim=0)[0]
-    scene_gs._xyz = norm_xyz
-    scene_gs._scaling = norm_scale
+    active_mask = (scene_gs.get_opacity > 0.9).squeeze()
+    inv_scale = (
+        orig_xyz[active_mask].max(dim=0)[0] - orig_xyz[active_mask].min(dim=0)[0]
+    ).max()
+    norm_scale = orig_scale / inv_scale
+    norm_xyz = orig_xyz / inv_scale
+
+    if outlier_percentile is None:
+        lower_bound_xyz = torch.min(norm_xyz[active_mask], dim=0)[0]
+        upper_bound_xyz = torch.max(norm_xyz[active_mask], dim=0)[0]
+    else:
+        lower_bound_xyz = torch.quantile(
+            norm_xyz[active_mask],
+            outlier_percentile,
+            dim=0,
+        )
+        upper_bound_xyz = torch.quantile(
+            norm_xyz[active_mask],
+            1.0 - outlier_percentile,
+            dim=0,
+        )
+
+    center = (lower_bound_xyz + upper_bound_xyz) / 2
+    norm_xyz = norm_xyz - center
+    scene_gs.from_xyz(norm_xyz)
+    scene_gs.mininum_kernel_size /= inv_scale.item()
+    scene_gs.from_scaling(norm_scale)
     return scene_gs
 
 
@@ -265,20 +261,48 @@ def make_scene(*outputs, in_place=False):
         outputs = [deepcopy(output) for output in outputs]
 
     all_outs = []
+    minimum_kernel_size = float("inf")
     for output in outputs:
         # move gaussians to scene frame of reference
         PC = SceneVisualizer.object_pointcloud(
-            points_local=(output["gaussian"][0]._xyz - 0.5).unsqueeze(0),
+            points_local=output["gaussian"][0].get_xyz.unsqueeze(0),
             quat_l2c=output["rotation"],
             trans_l2c=output["translation"],
             scale_l2c=output["scale"],
         )
-        output["gaussian"][0]._xyz = PC.points_list()[0]
-        output["gaussian"][0]._scaling *= output["scale"]
+        output["gaussian"][0].from_xyz(PC.points_list()[0])
+        # must ... ROTATE
+        output["gaussian"][0].from_rotation(
+            quaternion_multiply(
+                quaternion_invert(output["rotation"]),
+                output["gaussian"][0].get_rotation,
+            )
+        )
+        scale = output["gaussian"][0].get_scaling
+        adjusted_scale = scale * output["scale"]
+        assert (
+            output["scale"][0, 0].item()
+            == output["scale"][0, 1].item()
+            == output["scale"][0, 2].item()
+        )
+        output["gaussian"][0].mininum_kernel_size *= output["scale"][0, 0].item()
+        adjusted_scale = torch.maximum(
+            adjusted_scale,
+            torch.tensor(
+                output["gaussian"][0].mininum_kernel_size * 1.1,
+                device=adjusted_scale.device,
+            ),
+        )
+        output["gaussian"][0].from_scaling(adjusted_scale)
+        minimum_kernel_size = min(
+            minimum_kernel_size,
+            output["gaussian"][0].mininum_kernel_size,
+        )
         all_outs.append(output)
 
     # merge gaussians
     scene_gs = all_outs[0]["gaussian"][0]
+    scene_gs.mininum_kernel_size = minimum_kernel_size
     for out in all_outs[1:]:
         out_gs = out["gaussian"][0]
         scene_gs._xyz = torch.cat([scene_gs._xyz, out_gs._xyz], dim=0)
@@ -330,18 +354,31 @@ def load_image(path):
 
 def load_mask(path):
     mask = load_image(path)
-    mask = mask > 127
-    mask = mask[..., 0]
+    mask = mask > 0
+    if mask.ndim == 3:
+        mask = mask[..., -1]
     return mask
 
 
-def load_all_masks(folder_path):
-    idx = 0
+def load_single_mask(folder_path, index=0, extension=".png"):
+    masks = load_masks(folder_path, [index], extension)
+    return masks[0]
+
+
+def load_masks(folder_path, indices_list=None, extension=".png"):
     masks = []
-    while os.path.exists(mask_path := os.path.join(folder_path, f"{idx}.jpg")):
+    indices_list = [] if indices_list is None else list(indices_list)
+    if not len(indices_list) > 0:  # get all all masks if not provided
+        idx = 0
+        while os.path.exists(os.path.join(folder_path, f"{idx}{extension}")):
+            indices_list.append(idx)
+            idx += 1
+
+    for idx in indices_list:
+        mask_path = os.path.join(folder_path, f"{idx}{extension}")
+        assert os.path.exists(mask_path), f"Mask path {mask_path} does not exist"
         mask = load_mask(mask_path)
         masks.append(mask)
-        idx += 1
     return masks
 
 
@@ -359,7 +396,7 @@ def display_image(image, masks=None):
         mask_union = np.zeros_like(image[..., :3])
         for i, mask in enumerate(masks):
             mask_display[mask] = mask_colors[i]
-            mask_union |= mask[..., None]
+            mask_union |= mask[..., None] if mask.ndim == 2 else mask
         imshow(black_image, axes[0, 1])
         imshow(mask_display, axes[1, 0])
         imshow(image * mask_union, axes[1, 1])
