c68d921cf97ac7c0e2ae674e32c4b32ed9a7c0a2
wilkenlea <lea.wilken.inbox@gmail.com>
2025-11-18 18:05:32 -0800

    Update demo.py with code from main README

--- demo.py
+++ demo.py
@@ -11,9 +11,7 @@ inference = Inference(config_path, compile=False)
 
 # load image (RGBA only, mask is embedded in the alpha channel)
 image = load_image("notebook/images/shutterstock_stylish_kidsroom_1640806567/image.png")
-mask = load_single_mask(
-    "notebook/images/shutterstock_stylish_kidsroom_1640806567", index=14
-)
+mask = load_single_mask("notebook/images/shutterstock_stylish_kidsroom_1640806567", index=14)
 
 # run model
 output = inference(image, mask, seed=42)
