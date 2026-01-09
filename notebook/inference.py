dfb5382250612c6b05d7c7dfa17128a4998245a4
RocKamuS <149111224+RocKamuS@users.noreply.github.com>
2026-01-09 16:40:22 +0300

    Revert "Colab Support and Environment Fixes"

--- notebook/inference.py
+++ notebook/inference.py
@@ -2,8 +2,7 @@
 import os
 
 # not ideal to put that here
-if "CUDA_HOME" not in os.environ and "CONDA_PREFIX" in os.environ:
-    os.environ["CUDA_HOME"] = os.environ["CONDA_PREFIX"]
+os.environ["CUDA_HOME"] = os.environ["CONDA_PREFIX"]
 os.environ["LIDRA_SKIP_INIT"] = "true"
 
 import sys
